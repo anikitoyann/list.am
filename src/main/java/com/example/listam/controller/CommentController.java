bdf1aa68d9deba25380bcc5b5a6baac05e886ffe
Ani <anikitoyan11@gamil.com>
2023-05-10 15:57:28 +0400

    Spring Boott

--- src/main/java/com/example/listam/controller/CommentController.java
+++ src/main/java/com/example/listam/controller/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -47,7 +48,7 @@ public class CommentController {
             commentRepository.save(comment1);
             return "redirect:/comments";
         } else {
-            return "redirect:/items";
+            return "redirect:/comments";
         }
     }
 
